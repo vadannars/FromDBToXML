aa51425888564fd897272528dec6a84adc9907b4
Martin <126289524+vadannars@users.noreply.github.com>
2025-07-02 14:52:46 +0000

    projekts delmål

--- index.php
+++ index.php
@@ -4,6 +4,31 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
+/*
+PROJEKTETS DELMÅL:
+
+* Skapa testmiljö i codespaces - KLART
+* Skapa XML - KLART
+* Hur loopa igenom apisvar?
+* Hur loopa skapandet av xml?
+* Hur ta parametrar?
+* Hur kontakta API?
+
+
+API:er att testa emot:
+https://petstore.swagger.io/
+https://gorest.co.in/
+
+Samplekod för att generera en xml:
+$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><products></products>');
+
+$product = $xml->addChild('product');
+$product->addChild('name','Product 1');
+$product->addChild('price','19.99');
+
+$xml->asXML('products.xml')
+
+*/
 
 function sayHello($name) {
 	echo "Hello $name!";
