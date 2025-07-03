86e2c5a23a79d07a2dc2258fa05c1ce370c4f6c7
Martin <126289524+vadannars@users.noreply.github.com>
2025-07-03 09:13:43 +0000

    comp upd w guzzle

--- index.php
+++ index.php
@@ -12,7 +12,26 @@ PROJEKTETS DELMÅL:
 * Hur loopa igenom apisvar?
 * Hur loopa skapandet av xml?
 * Hur ta parametrar?
+	// URL: http://example.com?name=Alice
+	$name = $_GET['name'] ?? 'Guest'; // Default to 'Guest' if 'name' is not set
+	echo "Hello, " . htmlspecialchars($name) . "!"; // Output: Hello, Alice!
+
 * Hur kontakta API?
+	* Kodmässigt php
+		
+		require 'vendor/autoload.php';
+
+		use GuzzleHttp\Client;
+
+		$client = new Client();
+		$response = $client->request('GET', 'https://api.example.com/data', [
+			'query' => ['param1' => 'value1', 'param2' => 'value2']
+		]);
+
+		$data = json_decode($response->getBody(), true); // Decode JSON response
+		print_r($data);
+
+	* Vilken adress till faktiska bibliotekets api-ingång?
 
 
 API:er att testa emot:
@@ -29,6 +48,16 @@ $product->addChild('price','19.99');
 $xml->asXML('products.xml')
 
 */
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+$client = new Client();
+
+function callAPI() {
+
+
+}
 
 function sayHello($name) {
 	echo "Hello $name!";
